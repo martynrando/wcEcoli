217f550a4f8b99e32827ffdbf53b95a9195a3c1f
martynrando <eointomas.martyn@gmail.com>
2025-12-08 12:55:19 +1100

    quick fix for variants import

--- docs/webdocs/fw_logic_and_launch.py
+++ docs/webdocs/fw_logic_and_launch.py
@@ -1,4 +1,34 @@
-from models.ecoli.sim.variants import variants
+#from models.ecoli.sim.variants import variants
+
+variants = [
+	'aa_synthesis_ko',
+	'aa_synthesis_ko_shift',
+	'aa_synthesis_sensitivity',
+	'aa_uptake_sensitivity',
+	'add_one_aa',
+	'add_one_aa_shift',
+	'condition',
+	'gene_knockout',
+	'mene_params',
+	'metabolism_kinetic_objective_weight',
+	'metabolism_secretion_penalty',
+	'new_gene_internal_shift',
+	'param_sensitivity',
+	'ppgpp_conc',
+	'ppgpp_limitations',
+	'ppgpp_limitations_ribosome',
+	'remove_aa_inhibition',
+	'remove_aas_shift',
+	'remove_one_aa',
+	'remove_one_aa_shift',
+	'rrna_operon_knockout',
+	'rrna_location',
+	'rrna_orientation',
+	'tf_activity',
+	'time_step',
+	'timelines',
+	'wildtype',
+	]
 
 
 WF_RULES = {
