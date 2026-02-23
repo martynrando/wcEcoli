d5179a825b00e022acaedab7686f3903316c8e16
martynrando <eointomas.martyn@gmail.com>
2026-02-23 14:12:43 +1100

    trying without reset

--- docs/webdocs/app.py
+++ docs/webdocs/app.py
@@ -83,7 +83,7 @@ print("")
 print("Created {} with the information provided.".format(my_launchpad))
 print("Created {} with the information provided.".format(my_qadapter))
 lpad = LaunchPad.from_file(my_launchpad)
-lpad.reset()
+
 
 
 # Home page
