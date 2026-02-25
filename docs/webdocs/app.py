75ac0fe285aa136e608d8b38d6f8dfbd588de223
martynrando <eointomas.martyn@gmail.com>
2026-02-25 15:20:34 +1100

    testing out dir

--- docs/webdocs/app.py
+++ docs/webdocs/app.py
@@ -316,6 +316,7 @@ def workflow_status_page():
 dash_data = AnalysisInteractive().parse_data_structure(
     path = '/user/out/20260225.021222__testing!'# simulation output path
 )
+print(os.listdir('/user/out/20260225.021222__testing!'))
 dash_app = create_app(
     data_structure=dash_data,
     app=dash.Dash(__name__, server=app, url_base_pathname='/analysis/')
